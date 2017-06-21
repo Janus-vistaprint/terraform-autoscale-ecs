22e86190c5c45e3de28500ec9a3409523660364d
Jon Charette <jon.charette@gmail.com>
2017-06-21 07:59:23 -0400

    Update alb_listener.tf
    Emergency fix.  Allowing HTTPS to ALB, but HTTP to container.

--- tf_ecs_default_service/alb_listener.tf
+++ tf_ecs_default_service/alb_listener.tf
@@ -3,7 +3,7 @@
 resource "aws_alb_target_group" "grp" {
   name                 = "${var.task_name}-${var.environment}"
   port                 = "${var.container_port}"
-  protocol             = "${var.protocol}"
+  protocol             = "${var.conatiner_protocol}"
   vpc_id               = "${var.vpc_id}"
   deregistration_delay = "${var.deregistration_delay}"
 
